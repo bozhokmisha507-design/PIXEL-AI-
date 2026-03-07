69dec4f646efea6137df60829d69a127057b3afd
Michael Bozhok <mihail_bozhok@mail.ru>
2026-03-07 21:22:47 +0300

    Исправление ошибок

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     # ЮMoney
     YOOMONEY_ACCESS_TOKEN = os.getenv("YOOMONEY_ACCESS_TOKEN")
     YOOMONEY_WALLET = os.getenv("YOOMONEY_WALLET", "")
-    PRICE_PER_GENERATION = 1
+    PRICE_PER_GENERATION = 2
 
     # Telegram Payments (на будущее)
     PAYMENT_PROVIDER_TOKEN = os.getenv("PAYMENT_PROVIDER_TOKEN", "")
